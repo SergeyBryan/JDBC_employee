c464984fdcf49ac5e1af1688aac296a813469154
sergeybryan <regkmastis@yandex.ru>
2023-05-27 20:19:44 +0300

    Домашняя работа по JDBC

---
+++ src/main/java/com/example/jdbc_employee/JdbcEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbc_employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcEmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jdbc_employee/JdbcEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbc_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
